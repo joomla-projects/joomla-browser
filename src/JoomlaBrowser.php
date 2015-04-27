6595605794bdfb899ddecef7f51f640801d5bd44
javier gomez <javier.gomez.corio@gmail.com>
2015-04-27 19:57:00 +0200

    Add debug messages

--- src/JoomlaBrowser.php
+++ src/JoomlaBrowser.php
@@ -58,37 +58,58 @@ class JoomlaBrowser extends WebDriver
         $this->debug('I open Joomla Installation Configuration Page and fill the fields');
 
         // I Wait for the text Main Configuration, meaning that the page is loaded
+        $this->debug('I for Main Configuration');
         $I->waitForText('Main Configuration', 10, 'h3');
 
+        $this->debug('I click Language Selector');
         $I->click("//div[@id='jform_language_chzn']/a"); // Language Selector
+        $this-debug('I select en-GB');
         $I->click("//li[text()='English (United Kingdom)']"); // English (United Kingdom)
         sleep(1);
+        $this-debug('I fill Site Name');
         $I->fillField('Site Name', 'Joomla CMS test');
+        $this-debug('I fill Site Description');
         $I->fillField('Description', 'Site for testing Joomla CMS');
 
         // I get the configuration from acceptance.suite.yml (see: tests/_support/acceptancehelper.php)
+        $this-debug('I fill Admin Email');
         $I->fillField('Admin Email', $this->config['admin email']);
+        $this-debug('I fill Admin Username');
         $I->fillField('Admin Username', $this->config['username']);
+        $this-debug('I fill Admin Password');
         $I->fillField('Admin Password', $this->config['password']);
+        $this-debug('I fill Admin Password Confirmation');
         $I->fillField('Confirm Admin Password', $this->config['password']);
+        $this-debug('I click Site Offline: no');
         $I->click("//fieldset[@id='jform_site_offline']/label[2]"); // ['No Site Offline']
+        $this-debug('I click Next');
         $I->click('Next');
 
         $this->debug('I Fill the form for creating the Joomla site Database');
         $I->waitForText('Database Configuration', 10, 'h3');
 
+        $this-debug('I select MySQLi');
         $I->selectOption('#jform_db_type', $this->config['database type']);
+        $this-debug('I fill Database Host');
         $I->fillField('Host Name', $this->config['database host']);
+        $this-debug('I fill Database User');
         $I->fillField('Username', $this->config['database user']);
+        $this-debug('I fill Database Password');
         $I->fillField('Password', $this->config['database password']);
+        $this-debug('I fill Database Name');
         $I->fillField('Database Name', $this->config['database name']);
+        $this-debug('I fill Database Prefix');
         $I->fillField('Table Prefix', $this->config['database prefix']);
+        $this-debug('I click Remove Old Database ');
         $I->click("//label[@for='jform_db_old1']"); // Remove Old Database button
+        $I->click('Next');
+        $this-debug('I click Next');
         $I->click('Next');
 
         $this->debug('I install joomla with or without sample data');
         $I->waitForText('Finalisation', 10, 'h3');
 
+        $this-debug('I install Sample Data');
         if ($this->config['install sample data']) :
             $I->selectOption('#jform_sample_file', $this->config['sample data']);
         else :
