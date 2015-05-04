62934535b7931788701b1ddcf63ab8aa61e4eae3
javier gomez <javier.gomez.corio@gmail.com>
2015-05-04 10:56:38 +0200

    Add debug message for Extension installer method

--- src/JoomlaBrowser.php
+++ src/JoomlaBrowser.php
@@ -195,6 +195,7 @@ class JoomlaBrowser extends WebDriver
         // @todo: we need to find a better locator for the following Install button
         $I->click(['xpath' => "//input[contains(@onclick,'Joomla.submitbutton3()')]"]); // Install button
         $I->waitForText('was successful', 10, ['id' => 'system-message-container']);
+        $this->debug('Extension successfully installed from' . $path);
     }
 
     /**
