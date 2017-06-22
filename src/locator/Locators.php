efef9dfcea01052e1c262fc3f0e8ad1b924544ea
puneet0191 <puneet.0191@gmail.com>
2017-06-23 00:01:53 +0530

    Updating Page Class, Non Static

--- src/locator/Locators.php
+++ src/locator/Locators.php
@@ -1,29 +1,30 @@
 <?php
+namespace Joomla\Browser\Locators;
 
-class Locators{
+class Locators {
 
-	public static $loginUserName = ['id' => 'username'];
+	public $loginUserName = ['id' => 'username'];
 
-	public static $loginPassword = ['id' => 'password'];
+	public $loginPassword = ['id' => 'password'];
 
-	public static $loginButton = ['xpath' => "//div[@class='login']/form/fieldset/div[4]/div/button"];
+	public $loginButton = ['xpath' => "//div[@class='login']/form/fieldset/div[4]/div/button"];
 
-	public static $frontEndLoginSuccess = ['xpath' => "//form[@id='login-form']/div[@class='logout-button']"];
+	public $frontEndLoginSuccess = ['xpath' => "//form[@id='login-form']/div[@class='logout-button']"];
 
-	public static $frontEndLogoutButton = ['xpath' => "//div[@class='logout']//button[contains(text(), 'Log out')]"];
+	public $frontEndLogoutButton = ['xpath' => "//div[@class='logout']//button[contains(text(), 'Log out')]"];
 
-	public static $frontEndLoginForm = ['xpath' => "//div[@class='login']//button[contains(text(), 'Log in')]"];
+	public $frontEndLoginForm = ['xpath' => "//div[@class='login']//button[contains(text(), 'Log in')]"];
 
-	public static $adminLoginPageUrl = '/administrator/index.php';
+	public $adminLoginPageUrl = '/administrator/index.php';
 
-	public static $adminLoginUserName = ['id' => 'mod-login-username'];
+	public $adminLoginUserName = ['id' => 'mod-login-username'];
 
-	public static $adminLoginPassword = ['id' => 'mod-login-password'];
+	public $adminLoginPassword = ['id' => 'mod-login-password'];
 
-	public static $adminLoginButton = ['xpath' => "//button[contains(normalize-space(), 'Log in')]"];
+	public $adminLoginButton = ['xpath' => "//button[contains(normalize-space(), 'Log in')]"];
 
-	public static $controlPanelLocator = ['css' => 'h1.page-title'];
+	public $controlPanelLocator = ['css' => 'h1.page-title'];
 
-	public static $frontEndLoginUrl = '/index.php?option=com_users&view=login';
+	public $frontEndLoginUrl = '/index.php?option=com_users&view=login';
 
 }
