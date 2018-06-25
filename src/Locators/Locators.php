df7a4c78b9c6944adcd5a71864a695e1c6626b3e
Samarth <Samarthsharma351@gmail.com>
2018-06-25 20:51:15 +0530

    Update Locators.php

--- src/Locators/Locators.php
+++ src/Locators/Locators.php
@@ -129,4 +129,60 @@ class Locators
 	 * @since  3.7.5
 	 */
 	public $adminControlPanelText = 'Control Panel';
+	
+	/**
+	 * Admin Module URL
+	 *
+	 * @var    array
+	 * @since  3.7.5
+	 */	
+	public static $moduleUrl = '/administrator/index.php?option=com_modules';
+	
+	/**
+	 * Select Module Title
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $moduleTitle = ['id' => 'jform_title'];
+	
+	/**
+	 * Select Filter Options
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $filterOptions = ['link' => 'Filtering Options'];
+	
+	/**
+	 * Select Filter Options
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $selectModuleCategory = ['id' => 'jform_params_catid'];
+	
+	/**
+	 * Drop Down Toggle Element.
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $dropDownToggle = ['xpath' => "//button[contains(@class, 'dropdown-toggle')]"];
+	
+	/**
+	 * Fill Category
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $fillModuleCategory = ['xpath' => '//*[@id="jform_position_chzn"]/div/div/input'];
+	
+	/**
+	 * Select
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public static $moduleSelect = ['id' => 'jform_params_parent_select'];
 }
