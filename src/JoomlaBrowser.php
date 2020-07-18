dc6a32a2b162e90e8c270d7aadbadc1aff3c7b18
Sven Bluege <svenbluege@gmail.com>
2020-07-18 09:22:00 +0200

    Wait until the install page gets interactive. Clicking to early on the tab will not cause a tab switch.

--- src/JoomlaBrowser.php
+++ src/JoomlaBrowser.php
@@ -498,6 +498,8 @@ class JoomlaBrowser extends WebDriver
 	{
 		$this->amOnPage('/administrator/index.php?option=com_installer');
 		$this->waitForText('Extensions: Install', '30', array('css' => 'H1'));
+		// the page needs some time to get interactive
+		$this->wait(1);
 		$this->click(array('link' => 'Install from URL'));
 		$this->debug('I enter the url');
 		$this->fillField(array('id' => 'install_url'), $url);
